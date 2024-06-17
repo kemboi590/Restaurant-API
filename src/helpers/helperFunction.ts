445365a35ec359344c631cc79614f197878ba690
kemboi590 <bkemboi590@gmail.com>
2024-06-17 23:58:41 +0300

    added ejs imports

--- src/helpers/helperFunction.ts
+++ src/helpers/helperFunction.ts
@@ -1,11 +1,12 @@
 import nodemailer from 'nodemailer';
+import ejs from 'ejs';
+import path from 'path';
 
-export const sendEmail = async (email: string, subject: string, userName: string, message: string) => {
-    // https://nodemailer.com/usage/using-gmail/
+export const sendEmail = async (email: string, subject: string, userName: string, message: string, html: string) => {
     try {
         const transporter = nodemailer.createTransport({
             service: 'gmail',
-            secure: true, // use SSL
+            secure: true,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASSWORD
@@ -17,19 +18,19 @@ export const sendEmail = async (email: string, subject: string, userName: string
             to: email,
             subject: `${subject}`,
             text: message,
-            html: `<b>${message}</b>`
+            html: html
         };
 
         const mailRes = await transporter.sendMail(mailOptions);
         console.log('mailRes', mailRes);
 
-        let mailResponse:string = '';
+        let mailResponse: string = '';
         if (mailRes.accepted.length > 0) {
             mailResponse = 'Email sent successfully';
         } else if (mailRes.rejected.length > 0) {
             mailResponse = 'Email not sent';
         } else {
-            mailResponse = 'Email server error sent';
+            mailResponse = 'Email server error';
         }
         return mailResponse;
     } catch (error: any) {
@@ -37,29 +38,17 @@ export const sendEmail = async (email: string, subject: string, userName: string
     }
 };
 
-export interface EmailOptions {
-    from: string;
-    to: string;
-    email: string;
-    subject: string;
-    text: string;
-    html: string;
-}
-
-export interface MailResponse {
-    accepted: string[];
-    rejected: string[];
-}
-
-export const sendRegistrationEmailTemplate = async (userEmail: string, eventName: string, UserName: string): Promise<string> => {
+export const sendRegistrationEmailTemplate = async (userEmail: string, eventName: string, UserName: string, imageUrl: string): Promise<string> => {
     try {
-        const subject: string = ` Confirmation, you have  ${eventName}`;
-        const message: string = `Hello 👋🏻 ${UserName},  We hope you enjoy our services. Thank you for choosing us.`;
+        const templatePath = path.join(__dirname, './emailTemplate.ejs');
+        const html: string = await ejs.renderFile(templatePath, { UserName, eventName, imageUrl });
 
-        const mailRes: string = await sendEmail(userEmail, subject, UserName, message);
+        const subject: string = `Confirmation: You have ${eventName}`;
+        const message: string = `Hello 👋🏻 ${UserName}, we hope you enjoy our services. Thank you for choosing us.`;
+
+        const mailRes: string = await sendEmail(userEmail, subject, UserName, message, html);
         return mailRes;
     } catch (error: any) {
         return JSON.stringify(error.message, null, 500);
     }
-
-}
+};
